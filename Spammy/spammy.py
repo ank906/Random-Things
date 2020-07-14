27a0c8414f4789956279b9ac701d7e81a2ce07cb
ank906 <benmau2005@aol.com>
2020-07-14 15:36:43 -0400

    Update spammy.py

--- Spammy/spammy.py
+++ Spammy/spammy.py
@@ -3,7 +3,7 @@ import pyautogui
 
 word = input("what am i spamming this time? : ")
 count = 0
-countmax = int(input("how many : "))
+countmax = int(input("how many times : "))
 delay = int(input("how long is the delay (in seconds 0 by default)? : "))
 time.sleep(1)
 print("starting in : 3")
